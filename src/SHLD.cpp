277a80510a8807d3b7ba25951ac0105ac095d8b0
Parth <parthendo@geu.ac.in>
2020-11-20 00:14:40 +0530

    bug fix #2

--- src/SHLD.cpp
+++ src/SHLD.cpp
@@ -2,12 +2,12 @@
 #include "tools.h"
 void SHLD (string arg1, string registers[],bool flag[], map<string,string> &memory){
 	int l1=arg1.length();
-	if(l1 == 4 && validityAddress(arg1) && validityAddress(increaseAddress(arg1)) && memory.empty() == false){
+	if(l1 == 4 && validityAddress(arg1) && registers[6]!="" && registers[5]!="" && memory.empty() == false){
 		memory[arg1]=registers[6];
 		memory[increaseAddress(arg1)]=registers[5];
 	}
 	else{
-		cout<<"Error: "<<arg1<<"\nInvalid address\nThe program will quit\n";
+		cout<<"Error: "<<arg1<<"\nInvalid content\nThe program will quit\n";
 		exit(0);
 	}
 }
