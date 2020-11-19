d032389093c49aeefe04760654c102981829187e
Parth <parthendo@geu.ac.in>
2020-11-20 00:08:54 +0530

    bug fix

--- src/SHLD.cpp
+++ src/SHLD.cpp
@@ -2,7 +2,7 @@
 #include "tools.h"
 void SHLD (string arg1, string registers[],bool flag[], map<string,string> &memory){
 	int l1=arg1.length();
-	if(l1 == 4 && validityAddress(arg1) && validityAddress(increaseAddress(arg1))){
+	if(l1 == 4 && validityAddress(arg1) && validityAddress(increaseAddress(arg1)) && memory.empty() == false){
 		memory[arg1]=registers[6];
 		memory[increaseAddress(arg1)]=registers[5];
 	}
